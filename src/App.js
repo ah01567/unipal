7e522bcccdeea81c276d40e2415fdee920b86d4f
Ahmed Henine <ah01567@gmail.com>
2023-06-09 16:54:09 +0100

    Added URL routes to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+
+
 function App() {
   return (
-    <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
