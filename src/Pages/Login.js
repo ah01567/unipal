5b6d0b1a91e3752f0399636aa1502d379cc9b673
Ahmed Henine <ah01567@gmail.com>
2023-06-15 14:32:55 +0100

    Added login system to the app

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react';
+import {  signInWithEmailAndPassword   } from 'firebase/auth';
+import { auth } from './Firebase';
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -19,14 +22,28 @@ function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const [error, setError] = useState();
+
+  const navigate = useNavigate(); 
+  const LoginFunction = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/")
+        console.log(user);
+    })
+    .catch((error) => {
+       if (error.code === "auth/user-not-found") {
+            setError("This email address is not valid. please register first");
+         } else if (error.code === "auth/wrong-password") {
+            setError("Incorrect password. Try again");
+        } else if (error.code === "auth/invalid-email") {
+            setError("This email is not valid. Please check your email");
+        }
+     })
+    };
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -46,7 +63,7 @@ function Login() {
           <Typography component="h1" variant="h5">
              <b>Login</b> form:
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={LoginFunction} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
