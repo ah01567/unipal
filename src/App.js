c37b00b2fba1a1ba58c8675c554d6ba99bac826b
Ahmed Henine <ah01567@gmail.com>
2023-06-15 15:57:18 +0100

    Added URL access restrictions to the app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import useAuth from './Pages/CurrentUser';
 
 function App() {
+  const { currentUser } = useAuth();
+
+  const PrivateRoute = ({ element, path }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" replace />;
+    }
+    return <Routes><Route path={path} element={element}/></Routes>;
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-      </Routes>
+        <Route
+          path="/"
+          element={currentUser ? <PrivateRoute element={<Home />} /> : <Navigate to="/login" replace />}
+        />      
+        </Routes>
     </Router>
   );
 }
