637040234176df1c1393a0146a08389505cf50c4
Ahmed Henine <ah01567@gmail.com>
2023-06-15 16:09:10 +0100

    Add logout functionality to the app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
-import useAuth from './Pages/CurrentUser';
 
 function App() {
-  const { currentUser } = useAuth();
-
-  const PrivateRoute = ({ element, path }) => {
-    if (!currentUser) {
-      return <Navigate to="/login" replace />;
-    }
-    return <Routes><Route path={path} element={element}/></Routes>;
-  };
-
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/"
-          element={currentUser ? <PrivateRoute element={<Home />} /> : <Navigate to="/login" replace />}
-        />      
-        </Routes>
+      </Routes>
     </Router>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from "./CurrentUser";
+import {  signOut } from "firebase/auth";
+import { auth } from '../Pages/Firebasee';
 
 function Home() {
+  const { currentUser } = useAuth();
+  const navigate = useNavigate();
+
+  if (!currentUser) {
+    // Redirect to login page if currentUser doesn't exist
+    navigate('/login');
+    return null;
+  }
+
+  const handleLogout = () => {               
+    signOut(auth).then(() => {
+    // Sign-out successful.
+        console.log("Signed out successfully")
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
   return (
-      <div>
-        Home page
-      </div>
+    <div>
+      Home page
+      <button onClick={handleLogout}>Logout</button>
+    </div>
   );
 }
 
